60f1c0fdfe95882ab66aac21d1da39a936eb70cc
Daniil Grubskii <alexamyrz@yandex.ru>
2026-04-05 14:00:00 +0300

    Add test Text

--- app/src/main/java/com/example/deli/MainScreen.kt
+++ app/src/main/java/com/example/deli/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,6 +22,7 @@ fun MainScreen(innerPadding: PaddingValues, onNavigate: () -> Unit) {
                 onNavigate ()
             }
         ) {
+            Text("Текст")
         }
     }
 
