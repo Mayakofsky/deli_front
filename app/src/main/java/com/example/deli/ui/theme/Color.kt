1b212beaca23ce1e10100bf41dd14ca8926f3e0c
contradei <deriglazovroman6@gmail.com>
2026-04-23 23:45:00 +0300

    color comments

--- app/src/main/java/com/example/deli/ui/theme/Color.kt
+++ app/src/main/java/com/example/deli/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.deli.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// светлые оттенки для тёмной темы
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+// тёмные оттенки для светлой темы
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
