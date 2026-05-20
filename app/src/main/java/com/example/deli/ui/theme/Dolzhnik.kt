faa907eb0591c88586ddf2ba4e52d1263b8eda9b
contradei <deriglazovroman6@gmail.com>
2026-05-21 00:02:35 +0300

    dolshnik sb type fw

--- app/src/main/java/com/example/deli/ui/theme/Dolzhnik.kt
+++ app/src/main/java/com/example/deli/ui/theme/Dolzhnik.kt
@@ -1,8 +1,16 @@
 package com.example.deli.ui.theme
 
+// модель данных одного должника
 data class Dolzhnik(
+    // имя должника
     val name: String,
+
+    // сумма долга
     val amount: String,
+
+    // дата дедлайна возврата долга
     val deadline: String,
+
+    // ссылка на фото должника, может быть пустой
     val photoUri: String? = null
 )
